55396ff9a587dec41ab6718507d05e97b003e007
sunyang9876 <sunyang9876@hotmail.com>
2020-09-28 16:57:30 +1000

    complete movie component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Movie from './components/Movie';
 
 const FEATURED_API = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1';
-const IMG_API = 'https://image.tmdb.org/t/p/w1280';
+
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query='
 
 function App() {
@@ -12,8 +12,8 @@ function App() {
     fetch(FEATURED_API).then(res => res.json()).then(data => setMovies(data.results));
   },[])
 
-  return <div>
-    {movies.map(movie=>(<Movie />))}
+  return <div className="movie_container">
+    {movies.length > 0 && movies.map(movie => (<Movie key={movie.id} {...movie} />))}
   </div>
 }
 
--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+const IMG_API = 'https://image.tmdb.org/t/p/w1280';
 
-const Movie = () => {
+const Movie = ({title,poster_path,overview,vote_average}) => {
   return <div className="movie">
-    this is a movie component
+    <div className="movie_header">
+      <img src={IMG_API + poster_path} alt={title} />
+      <div className="movie_info">
+        <h3>{title}</h3>
+        <span>{vote_average}</span>
+      </div>
+    </div>
+    <div className="movie-overview">
+      <h2>overview</h2>
+      <p>{overview}</p>
+    </div>
   </div>
 }
 
