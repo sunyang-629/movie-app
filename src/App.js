f85efc5056585b0ea122a19f6e34eb1845f21c8e
sunyang9876 <sunyang9876@hotmail.com>
2020-09-28 15:56:27 +1000

    fetch fetured api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Movie from './components/Movie';
 
+const FEATURED_API = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1';
+const IMG_API = 'https://image.tmdb.org/t/p/w1280';
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query='
+
 function App() {
-  const movies = ['1', '2', '3'];
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch(FEATURED_API).then(res => res.json()).then(data => setMovies(data.results));
+  },[])
+
   return <div>
     {movies.map(movie=>(<Movie />))}
   </div>
