b2b2e37dbda04e1c12f75f4fb7bfe0cc9f00ad36
sunyang9876 <sunyang9876@hotmail.com>
2020-09-30 13:42:25 +1000

    add tag

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -2,12 +2,22 @@ import React from 'react';
 const IMG_API = 'https://image.tmdb.org/t/p/w1280';
 
 const Movie = ({title,poster_path,overview,vote_average}) => {
+  const setVoteColor = (vote) => {
+    if (vote >= 8) {
+      return 'green'
+    } else if (vote >= 6) {
+      return 'orange'
+    } else {
+      return 'red'
+    }
+  }
+
   return <div className="movie">
     <div className="movie_header">
       <img src={IMG_API + poster_path} alt={title} />
       <div className="movie_info">
         <h3>{title}</h3>
-        <span>{vote_average}</span>
+        <span className={`tag ${setVoteColor(vote_average)}`}>{vote_average}</span>
       </div>
     </div>
     <div className="movie-overview">
